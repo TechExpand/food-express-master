5ec5285302e1913666c0a895609befbfaed57c77
tony <tony@issuefixers.com>
2021-01-07 09:58:19 -0700

    tnyse was here

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ void main() {
     return null;
   }
 
+// t was here
 
   Provider.debugCheckInvalidValueType = null;
   Admob.initialize(getAppId());
@@ -41,13 +42,12 @@ void main() {
       ChangeNotifierProvider<Utils>(
         create: (context) => Utils(),
       ),
-       ChangeNotifierProvider<AdmobService>(
+      ChangeNotifierProvider<AdmobService>(
         create: (context) => AdmobService(),
       ),
     ],
     child: StartApp(),
   ));
-  
 }
 
 class StartApp extends StatefulWidget {
@@ -58,11 +58,10 @@ class StartApp extends StatefulWidget {
   }
 }
 
-
 class StartAppState extends State<StartApp> {
   @override
   void initState() {
-       super.initState();
+    super.initState();
     StatusBar.color(Colors.blue);
     Provider.of<LocationService>(context, listen: false).getCurrentLocation();
     //Get current location every 5 minutes.
@@ -70,33 +69,33 @@ class StartAppState extends State<StartApp> {
       Provider.of<LocationService>(context, listen: false).getCurrentLocation();
     });
 
+    //Admob Advert Code
 
-
-     //Admob Advert Code
-
-      Provider.of<AdmobService>(context, listen: false).instatitialAd = AdmobInterstitial(
+    Provider.of<AdmobService>(context, listen: false).instatitialAd =
+        AdmobInterstitial(
       adUnitId: 'ca-app-pub-7014950727779735/7173204476',
-      listener: (AdmobAdEvent event, Map<String, dynamic> args){
-        if(event == AdmobAdEvent.closed){
-          Provider.of<AdmobService>(context, listen: false).instatitialAd.load();
+      listener: (AdmobAdEvent event, Map<String, dynamic> args) {
+        if (event == AdmobAdEvent.closed) {
+          Provider.of<AdmobService>(context, listen: false)
+              .instatitialAd
+              .load();
         }
+      },
+    );
+    Provider.of<AdmobService>(context, listen: false).instatitialAd.load();
 
-    },
-    ); 
-  Provider.of<AdmobService>(context, listen: false).instatitialAd.load();
-  
 //Show instatitialAds every 5 minutes.
-   Timer.periodic(Duration(minutes: 4), (timer) {
+    Timer.periodic(Duration(minutes: 4), (timer) {
       Provider.of<AdmobService>(context, listen: false).instatitialAd.show();
     });
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+    Provider.of<AdmobService>(context, listen: false).instatitialAd.dispose();
+  }
 
-@override
-void dispose(){
-  super.dispose();
-  Provider.of<AdmobService>(context, listen: false).instatitialAd.dispose();
-}
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
